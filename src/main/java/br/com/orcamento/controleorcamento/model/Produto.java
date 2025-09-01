01c5b8039b1b1bed696c4d9b0c32170dc94278fc
Renato <rbfcerqueira@hotmail.com>
2025-09-01 12:39:01 -0300

    Trabalho Criado e conexão com o Banco

---
+++ src/main/java/br/com/orcamento/controleorcamento/model/Produto.java
@@ -0,0 +1,95 @@
+package br.com.orcamento.controleorcamento.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+public class Produto {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public BigDecimal getTaxas() {
+        return taxas;
+    }
+
+    public void setTaxas(BigDecimal taxas) {
+        this.taxas = taxas;
+    }
+
+    public LocalDate getPrazoEntrega() {
+        return prazoEntrega;
+    }
+
+    public void setPrazoEntrega(LocalDate prazoEntrega) {
+        this.prazoEntrega = prazoEntrega;
+    }
+
+    public BigDecimal getDescontos() {
+        return descontos;
+    }
+
+    public void setDescontos(BigDecimal descontos) {
+        this.descontos = descontos;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private BigDecimal valor;
+
+    private BigDecimal taxas;
+
+    private LocalDate prazoEntrega;
+
+    private BigDecimal descontos;
+
+    private String observacoes;
+
+    @ManyToOne
+    private Fornecedor fornecedor;
+
+    // Getters e Setters serão gerados aqui
+
+}
